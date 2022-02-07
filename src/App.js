20f737a40abaff0d3a49240a20784e0fc44f548b
heejun <gmlwns2002@gmail.com>
2022-02-08 02:44:28 +0900

    diary API

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
 import { useRef, useState } from "react";
+import { useEffect } from "react/cjs/react.development";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "박희준",
-//     content: "하이 1",
-//     emotion: 2,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: "김영은",
-//     content: "하이 2",
-//     emotion: 1,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: "홍길동",
-//     content: "하이 3",
-//     emotion: 4,
-//     created_date: new Date().getTime(),
-//   },
-// ];
+//https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]); //일기데이터 배열을 저장할거기 때문에 배열을 초기값으로 함
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        //Math.random()*5는 0~4까지 수를 랜덤생성(소수점나올수있음), Math.Floor를 적용하여 정수가 나오도록 하고 +1을 하여 1~5까지의 수가 나오게함
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  });
 
   // 새로운 일기 추가하는 함수
   const onCreate = (author, content, emotion) => {
@@ -64,7 +65,6 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
